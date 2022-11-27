068f366a4e4b37ee2382bb6058a1f1e4bc39769c
JJDelight <74662048+JJDelight@users.noreply.github.com>
2022-11-26 22:23:25 -0500

    Added softOn/softOff functions, and delay
    GitHub won't connect to VS so I uploaded the files instead, softOff/On is a great demonstration on using the delay/time stopping function
    
    Cheers!

--- Qt_Project/mainwindow.h
+++ Qt_Project/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,12 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void softOn();
+    void softOff();
+    void delay(int seconds);// Function to delay a function for x seconds
+
+private slots:
+    void on_lightOne_clicked();
 
 private:
     Ui::MainWindow *ui;
